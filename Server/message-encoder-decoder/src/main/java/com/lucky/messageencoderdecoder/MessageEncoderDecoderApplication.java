c68f791d151a53aaa21fc4d3e6bb8ef28ab6a44a
Matas <luckyrads@gmail.com>
2022-11-30 11:03:08 +0200

    ADD: Sample server Java application with gradle.

---
+++ Server/message-encoder-decoder/src/main/java/com/lucky/messageencoderdecoder/MessageEncoderDecoderApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.messageencoderdecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageEncoderDecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageEncoderDecoderApplication.class, args);
+	}
+
+}
