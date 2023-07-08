c76061855518a41fff723320348b90fb650e2b1f
LuckyRads <luckyrads@gmail.com>
2023-07-08 22:38:43 +0300

    CHN: Init sql scripts.

--- Server/message-encoder-decoder/src/main/resources/sql/init-schema.sql
+++ Server/message-encoder-decoder/src/main/resources/sql/init-schema.sql
@@ -1,3 +1,3 @@
 DROP SCHEMA IF EXISTS `message-encoder-decoder`;
 
-CREATE SCHEMA `message-encoder-decoder` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE SCHEMA `message-encoder-decoder` DEFAULT CHARACTER SET utf8mb4;
--- Server/message-encoder-decoder/src/main/resources/sql/init-tables.sql
+++ Server/message-encoder-decoder/src/main/resources/sql/init-tables.sql
@@ -1,13 +1,11 @@
-DROP TABLE IF EXISTS messages, users, chats;
-
 CREATE TABLE
-    messages (
+    IF NOT EXISTS messages (
         id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
         message VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE
-    users (
+    IF NOT EXISTS users (
         id INT NOT NULL AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL,
         username VARCHAR(255) NOT NULL,
@@ -16,7 +14,7 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    chats (
+    IF NOT EXISTS chats (
         id INT NOT NULL AUTO_INCREMENT,
         user_id INT NOT NULL,
         message_id INT NOT NULL,
