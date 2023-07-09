b4746daa35bf09c4a85edddb94ab11d01bbffdc7
LuckyRads <luckyrads@gmail.com>
2023-07-09 03:04:59 +0300

    UPD: Conversations repository dummy dsl.

--- Server/message-encoder-decoder/src/main/java/com/lucky/messageencoderdecoder/infrastructure/repository/mariadb/JooqConversationRepository.java
+++ Server/message-encoder-decoder/src/main/java/com/lucky/messageencoderdecoder/infrastructure/repository/mariadb/JooqConversationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.lucky.messageencoderdecoder.domain.model.conversation.Conversation;
 import com.lucky.messageencoderdecoder.domain.model.conversation.values.ConversationId;
 import com.lucky.messageencoderdecoder.domain.repository.ConversationRepository;
+import com.lucky.messageencoderdecoder.generated.jooq.tables.records.ConversationsRecord;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,13 +20,30 @@ public class JooqConversationRepository implements ConversationRepository {
 
     @Override
     public Conversation getConversation(ConversationId id) {
-        return null;
+        var conversationsRecord = dslContext
+                .select(CONVERSATIONS.ID, CONVERSATIONS.USER_ID, CONVERSATIONS.MESSAGE_ID)
+                .from(CONVERSATIONS)
+                .where(CONVERSATIONS.ID.eq(id.getId().toString()))
+                .fetchOneInto(ConversationsRecord.class);
+        return new Conversation(new ConversationId(conversationsRecord.getId()));
     }
 
     @Override
     public void saveConversation(Conversation conversation) {
-        // TODO Auto-generated method stub
+        var optionalConversationsRecord = dslContext
+                .select(CONVERSATIONS.ID, CONVERSATIONS.USER_ID, CONVERSATIONS.MESSAGE_ID)
+                .from(CONVERSATIONS)
+                .where(CONVERSATIONS.ID.eq(conversation.getId().getId().toString()))
+                .fetchOptionalInto(ConversationsRecord.class);
 
+        var conversationsRecord = optionalConversationsRecord.orElseGet(() -> {
+            var newConversationsRecord = dslContext.newRecord(CONVERSATIONS);
+            newConversationsRecord.setId(conversation.getId().getId().toString());
+            return newConversationsRecord;
+        });
+        // TODO
+        conversationsRecord.setMessageId("");
+        conversationsRecord.setUserId("");
+        conversationsRecord.store();
     }
-
 }
